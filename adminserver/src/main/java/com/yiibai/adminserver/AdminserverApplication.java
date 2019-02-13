7528c3ba2d9ab218c5f9630a097ee4e6053f70dd
오용수 <ys.oh@tricycle.co.kr>
2019-02-13 12:19:18 +0900

    server & client

---
+++ adminserver/src/main/java/com/yiibai/adminserver/AdminserverApplication.java
@@ -0,0 +1,16 @@
+package com.yiibai.adminserver;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+@EnableAdminServer
+public class AdminserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AdminserverApplication.class, args);
+	}
+
+}
+
